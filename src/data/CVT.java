343a59d657bf1c713e645a43bd0a2ae8bc390f9e
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-22 10:33:09 +0530

    completed the task

---
+++ src/data/CVT.java
@@ -0,0 +1,23 @@
+package data;
+
+import data.Automatic;
+
+public class CVT extends Automatic {
+    public CVT(String transmissionType, String modelNo, int noOfGears) {
+        super(transmissionType, modelNo, noOfGears);
+    }
+    @Override
+    public void setGearRatio(double[] gearRatio) {
+        super.setGearRatio(gearRatio);
+    }
+
+    @Override
+    public double[] getGearRatio() {
+        return super.getGearRatio();
+    }
+
+    @Override
+    public void showSpecs() {
+        super.showSpecs();
+    }
+}
---
+++ src/data/DCT.java
@@ -0,0 +1,23 @@
+package data;
+
+import data.Automatic;
+
+public class DCT extends Automatic {
+    public DCT(String transmissionType, String modelNo, int noOfGears) {
+        super(transmissionType, modelNo, noOfGears);
+    }
+    @Override
+    public void setGearRatio(double[] gearRatio) {
+        super.setGearRatio(gearRatio);
+    }
+
+    @Override
+    public double[] getGearRatio() {
+        return super.getGearRatio();
+    }
+
+    @Override
+    public void showSpecs() {
+        super.showSpecs();
+    }
+}
